06829fa1396cc5353ba6c523f5cf40c760e07969
Dipesh Shrestha <dipstha444@gmail.com>
2022-07-13 10:24:39 -0400

    first commit

---
+++ example.js
@@ -0,0 +1,20 @@
+let day = 'sunday';
+
+switch(day){
+    case 'monday':
+        console.log('5 days to saturday');
+        break;
+     case 'tuesday':
+            console.log('4 days to saturday');
+            break;
+    case 'wednesday':
+             console.log('3 days to saturday');
+            break;
+    case 'thursday':
+            console.log('2 days to saturday');
+            break;
+    case 'friday':
+            console.log('1 days to saturday');
+                        
+            break;
+}
